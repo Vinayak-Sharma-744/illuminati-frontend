e5651dc2fed589d3bcb9e76e18278843d84f3513
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-08-26 16:29:12 +0530

    vercel

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://video-call-delta.vercel.app/');
+const socket = io('https://illuminati-backend.vercel.app/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
